24dc4ab32d8169accd76c75ad65d216a547875fe
Austen Jennings <austenhjennings@gmail.com>
2016-07-24 07:12:33 -0400

    Added test project

---
+++ PathfindingTests/BreadthFirstSearchTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pathfinding;
+
+namespace PathfindingTests
+{
+    [TestClass]
+    public class BreadthFirstSearchTests
+    {
+        private BreadthFirstSearch search;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            search = new BreadthFirstSearch();
+        }
+
+        [TestMethod]
+        public void AllSpacesOpen()
+        {
+            Map map = new Map("Level1");
+            var points = search.Search(map, new Point(0, 0));
+            Assert.IsNotNull(points);
+            Assert.AreEqual(100, points.Count);
+        }
+
+        [TestMethod]
+        public void LimitMoves()
+        {
+            Map map = new Map("Level1");
+            var points = search.Search(map, new Point(3, 3), 3);
+            Assert.IsNotNull(points);
+            Assert.AreEqual(1 + 4 + 4*2 + 4*3, points.Count);
+            Assert.IsNotNull(points.FirstOrDefault(point =>point.Key.Equals(new Point(3, 3)) && point.Value == 0));
+            Assert.IsNotNull(points.FirstOrDefault(point =>point.Key.Equals(new Point(3, 2)) && point.Value == 1));
+            Assert.IsNotNull(points.FirstOrDefault(point =>point.Key.Equals(new Point(4, 3)) && point.Value == 1));
+            Assert.IsNotNull(points.FirstOrDefault(point =>point.Key.Equals(new Point(3, 4)) && point.Value == 1));
+            Assert.IsNotNull(points.FirstOrDefault(point =>point.Key.Equals(new Point(2, 3)) && point.Value == 1));
+            Assert.IsNotNull(points.FirstOrDefault(point =>point.Key.Equals(new Point(3, 1)) && point.Value == 2));
+            Assert.IsNotNull(points.FirstOrDefault(point =>point.Key.Equals(new Point(4, 2)) && point.Value == 2));
+            Assert.IsNotNull(points.FirstOrDefault(point =>point.Key.Equals(new Point(5, 3)) && point.Value == 2));
+            Assert.IsNotNull(points.FirstOrDefault(point =>point.Key.Equals(new Point(4, 4)) && point.Value == 2));
+            Assert.IsNotNull(points.FirstOrDefault(point =>point.Key.Equals(new Point(3, 5)) && point.Value == 2));
+            Assert.IsNotNull(points.FirstOrDefault(point =>point.Key.Equals(new Point(2, 4)) && point.Value == 2));
+            Assert.IsNotNull(points.FirstOrDefault(point =>point.Key.Equals(new Point(1, 3)) && point.Value == 2));
+            Assert.IsNotNull(points.FirstOrDefault(point =>point.Key.Equals(new Point(2, 2)) && point.Value == 2));
+            Assert.IsNotNull(points.FirstOrDefault(point =>point.Key.Equals(new Point(3, 0)) && point.Value == 3));
+            Assert.IsNotNull(points.FirstOrDefault(point =>point.Key.Equals(new Point(4, 1)) && point.Value == 3));
+            Assert.IsNotNull(points.FirstOrDefault(point =>point.Key.Equals(new Point(5, 2)) && point.Value == 3));
+            Assert.IsNotNull(points.FirstOrDefault(point =>point.Key.Equals(new Point(6, 3)) && point.Value == 3));
+            Assert.IsNotNull(points.FirstOrDefault(point =>point.Key.Equals(new Point(5, 4)) && point.Value == 3));
+            Assert.IsNotNull(points.FirstOrDefault(point =>point.Key.Equals(new Point(4, 5)) && point.Value == 3));
+            Assert.IsNotNull(points.FirstOrDefault(point =>point.Key.Equals(new Point(3, 6)) && point.Value == 3));
+            Assert.IsNotNull(points.FirstOrDefault(point =>point.Key.Equals(new Point(2, 5)) && point.Value == 3));
+            Assert.IsNotNull(points.FirstOrDefault(point =>point.Key.Equals(new Point(1, 4)) && point.Value == 3));
+            Assert.IsNotNull(points.FirstOrDefault(point =>point.Key.Equals(new Point(0, 3)) && point.Value == 3));
+            Assert.IsNotNull(points.FirstOrDefault(point =>point.Key.Equals(new Point(1, 2)) && point.Value == 3));
+            Assert.IsNotNull(points.FirstOrDefault(point =>point.Key.Equals(new Point(2, 1)) && point.Value == 3));
+        }
+
+        [TestMethod]
+        public void IgnoresBlockedSpaces()
+        {
+            Map map = new Map("Level2");
+            var points = search.Search(map, new Point(0, 0), 10);
+            Assert.IsNotNull(points);
+            Assert.AreEqual(1, points.Count);
+            Assert.IsNotNull(points[new Point(0, 0)]);
+        }
+    }
+}
---
+++ PathfindingTests/MapTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pathfinding;
+
+namespace PathfindingTests
+{
+    [TestClass]
+    public class MapTests
+    {
+        private Map map;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            map = new Map("Level1");
+            Assert.IsNotNull(map);
+        }
+
+        [TestMethod]
+        public void LoadsMap()
+        {
+            map.LoadMap("Level2");
+            Assert.IsNotNull(map);
+            // Level1 is 10x10 so make so it can access very last
+            Assert.IsNotNull(map[9, 9]);
+        }
+
+        [TestMethod]
+        public void UsesCorrectXY()
+        {
+            // since in the grid, the first is actually Y and second X grid[y][x], make sure its
+            // being accessed right by looking for blocks in [x][y] specific points
+            map.LoadMap("Level2");
+            Assert.IsFalse(map[4, 0].IsPassable);
+            Assert.IsFalse(map[1, 8].IsPassable);
+        }
+
+        [TestMethod]
+        public void NeighborsAllInBounds()
+        {
+            List<Point> neighbors = map.Neighbors(new Point(3, 4)).ToList();
+            Assert.IsNotNull(neighbors);
+            Assert.AreEqual(4, neighbors.Count);
+            Assert.IsTrue(neighbors.Any(point => point.Equals(new Point(3, 3))));
+            Assert.IsTrue(neighbors.Any(point => point.Equals(new Point(4, 4))));
+            Assert.IsTrue(neighbors.Any(point => point.Equals(new Point(3, 5))));
+            Assert.IsTrue(neighbors.Any(point => point.Equals(new Point(2, 4))));
+        }
+
+        [TestMethod]
+        public void NeighbordsIgnoresOutOfBoundTiles()
+        {
+            List<Point> neighbors = map.Neighbors(new Point(0, 0)).ToList();
+            Assert.IsNotNull(neighbors);
+            Assert.AreEqual(2, neighbors.Count);
+            Assert.IsTrue(neighbors.Any(point => point.Equals(new Point(0, 1))));
+            Assert.IsTrue(neighbors.Any(point => point.Equals(new Point(1, 0))));
+        }
+
+        [TestMethod]
+        public void InBounds()
+        {
+            Point point = new Point(0, 0);
+            Assert.IsTrue(map.InBounds(point));
+            point = new Point(-1, 0);
+            Assert.IsFalse(map.InBounds(point));
+            point = new Point(250, 0);
+            Assert.IsFalse(map.InBounds(point));
+            point = new Point(0, 250);
+            Assert.IsFalse(map.InBounds(point));
+        }
+    }
+}
